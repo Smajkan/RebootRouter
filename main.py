23f3a8cd2c4fccb89e57d9278a2c12423c97a845
Aldin Smajkan <39434215+Smajkan@users.noreply.github.com>
2023-02-03 01:34:25 +0100

    Updated the functionality of the program
    1. Updated createLogFile() function
    2. Added has_wifi_card() - checks if there's WiFi support
    3. restart_wifi_adapterWin() - Tries to restart wifi adapter

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+#- The `createLogFile(restartsDone)` function creates a log file or writes to an existing log file the number of times the Wi-Fi adapter has been restarted today.
+#- The `writeToFile(WriteToFileResets, logFile)` function writes to the log file the time the Wi-Fi adapter was restarted and the number of restarts performed.
+#- The `currentTime()` function returns the current time.
+#- The `restart_wifi_adapterWin()` function restarts the Wi-Fi adapter on Windows, MacOS, or Linux depending on the operating system.
+#- The `has_wifi_card()` function returns a Boolean indicating whether the computer has a Wi-Fi network card.
+
+
 from checkconnection import *
 from seleniumtest import *
 import os.path
 from os import path
 from datetime import datetime
+import subprocess
+import ctypes
+import sys
+import platform
+import json
+
 
 
 # 200 = if user's not connected to internet at all / doesn't have internet enabled
@@ -13,21 +26,49 @@ current_status = None
 no_actions = 0
 restartsDone = 0
 
+#Check if the computer has a Wi-Fi network card. Returns True if a Wi-Fi card is found, 
+#False otherwise.
+def has_wifi_card():
+    OS = platform.system()
+    if OS == "Windows":
+        result = subprocess.run("netsh interface show interface", shell=True, stdout=subprocess.PIPE)
+        if "Wi-Fi" in result.stdout.decode():
+            return True
+        else:
+            return False
+    elif OS == "Darwin": # Mac OS
+        result = subprocess.run("networksetup -listallhardwareports", shell=True, stdout=subprocess.PIPE)
+        if "Wi-Fi" in result.stdout.decode():
+            return True
+        else:
+            return False
+    elif OS == "Linux":
+        result = subprocess.run("iwconfig", shell=True, stdout=subprocess.PIPE)
+        if "wlan" in result.stdout.decode():
+            return True
+        else:
+            return False
+    else:
+        print("Unsupported operating system")
+        return False
 
 #Currently in development since I realized that it'd be pretty handy if it finds available SSIDs and then checks 
 #if there's connection with the same name and then connect on it
-"""
-#This restarts WiFi
 def restart_wifi_adapterWin():
-    # scan available Wifi networks
-    os.system('cmd /c "netsh wlan show networks"')
- 
-    # input Wifi name
-    SSID_name = "SmajkanNET"
- 
-    # connect to the given wifi network
-    os.system(f'''cmd /c "netsh wlan connect name={SSID_name}"''')
-"""
+    if not has_wifi_card():
+        print("It seems like you don't have Wi-Fi card")
+    else:
+        OS = platform.system()
+        if OS == "Windows":
+            subprocess.run("netsh interface set interface \"Wi-Fi\" admin=enable", shell=True, check=True)
+        elif OS == "Darwin": # Mac OS
+            subprocess.run("networksetup -setairportpower airport on", shell=True, check=True)
+            subprocess.run("ifconfig en0 up", shell=True, check=True)
+        elif OS == "Linux":
+            subprocess.run("nmcli radio wifi on", shell=True, check=True)
+            subprocess.run("rfkill unblock wifi", shell=True, check=True)
+        else:
+            print("Unsupported operating system")
 
 #Vraca trenutno vrijeme
 def currentTime():
@@ -42,12 +83,34 @@ def writeToFile(WriteToFileResets,logFile):
 
 #This function is used to createLogFile, in case there's LogFile that exist it will just proceed with writing to file
 def createLogFile(restartsDone):
-    if path.exists("LogFile.txt") == True:  #U slucaju da vec postoji "LogFile.txt" nastavljamo praviti log na taj fajl
-        myLogFile = open("LogFile.txt","a")
-        writeToFile(restartsDone,myLogFile)
-    else: #U slucaju da log file ne postoji, kreiramo fajl i onda pisemo u njega
-        myLogFile = open("LogFile.txt","w+")
-        writeToFile(restartsDone,myLogFile)
+    try:
+        # Check if LogFile.txt exists
+        if os.path.exists("LogFile.txt"):
+            # Get the creation time of the file
+            creation_time = os.path.getctime("LogFile.txt")
+            # Convert the creation time to date format
+            file_date = datetime.fromtimestamp(creation_time).date()
+            # Get the current date
+            current_date = datetime.now().date()
+            # If the file creation date is equal to current date
+            if file_date == current_date:
+                # Open the file in append mode
+                with open("LogFile.txt", "a") as my_log_file:
+                    # Call the writeToFile function to write the restartsDone to the file
+                    writeToFile(restartsDone, my_log_file)
+            else:
+                # Open the file in write mode
+                with open("LogFile.txt", "w") as my_log_file:
+                    # Call the writeToFile function to write the restartsDone to the file
+                    writeToFile(restartsDone, my_log_file)
+        else:
+            # If the file does not exist, open the file in write mode
+            with open("LogFile.txt", "w") as my_log_file:
+                # Call the writeToFile function to write the restartsDone to the file
+                writeToFile(restartsDone, my_log_file)
+    except Exception as e:
+        print("An error occurred while creating the log file: ", str(e))
+
 
 #performAction: 1. Reboots router ; 2. Counts how many times restarts're done;
 def performAction1():
@@ -58,7 +121,7 @@ def performAction1():
 
 #Currently in development: Will try to add option to automatically re-enable connection and connect to available network    
 def performAction2():
-    #restart_wifi_adapterWin()
+    restart_wifi_adapterWin()
     print("Konekcija nije ostvarena")
 
 
